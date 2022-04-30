31206298d03f0622d159d1fc2dcefaacc90f42fd
manealucacristian <luca.cmanea@gmail.com>
2022-04-30 01:30:51 -0300

    ORM Se crearon los modelos y se realizaron las migraciones

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    #django automaticamente nos crea un id por eso no lo generamos nosotros
+    question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField("date published")
+
+
+
+class Choise(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default = 0)
+
+
+
