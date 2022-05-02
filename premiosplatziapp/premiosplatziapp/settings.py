8aede77c18e6728798052d8ecf69a072c56d06d7
manealucacristian <luca.cmanea@gmail.com>
2022-05-01 23:47:00 -0300

    Configurando variables de entorno

--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -1,4 +1,4 @@
-from .env import key
+from decouple import *
 """
 Django settings for premiosplatziapp project.
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
